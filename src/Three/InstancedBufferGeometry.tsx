fee62a08b2dd2c208b4b63c201c320ff82dc2643
Guillaume Gomez <guillaume.gomez846@gmail.com>
2025-12-11 13:54:21 +0100

    make a copy to manipulate the component smoothly

--- src/Three/InstancedBufferGeometry.tsx
+++ src/Three/InstancedBufferGeometry.tsx
@@ -1,10 +1,76 @@
 import React, {useMemo} from "react";
 import pack from "pack-spheres"
-import {SphereGeometry, GLSL3, InstancedBufferAttribute} from "three"
+import {SphereGeometry, GLSL3, BufferAttribute, InstancedBufferAttribute, InstancedBufferGeometry, Vector2} from "three"
 
-export default function instancedBufferGeometry(props) {
+interface instancedBufferGeometryProps {
+    width: number;
+    height: number
+}
 
-    const {
+export default function instancedBufferGeometry({width, height, ...props} : instancedBufferGeometryProps) {
+    // const numInstances = width * height;
+    // const objectData = useMemo(() => {
+    //     const numPoints = width * height;
+    //     const data = init(numPoints);
+    //     return data;
+    // }, [width, height]);
+
+    // function init(numPoints: number) {
+    //     const geometry = new InstancedBufferGeometry();
+        
+    //     // positions
+    //     const positions = new BufferAttribute(new Float32Array(4 * 3), 3);
+    //     positions.setXYZ(0, -0.5, 0.5, 0.0);
+    //     positions.setXYZ(1, 0.5, 0.5, 0.0);
+    //     positions.setXYZ(2, -0.5, -0.5, 0.0);
+    //     positions.setXYZ(3, 0.5, -0.5, 0.0);
+        
+    //     // uvs
+    //     const uvs = new BufferAttribute(new Float32Array(4 * 2), 2);
+    //     uvs.setXYZ(0, 0.0, 0.0);
+    //     uvs.setXYZ(1, 1.0, 0.0);
+    //     uvs.setXYZ(2, 0.0, 1.0);
+    //     uvs.setXYZ(3, 1.0, 1.0);
+
+    //     // index
+    //     const indexes = new BufferAttribute(new Uint16Array([ 0, 2, 1, 2, 3, 1 ]), 1);
+
+    //     const indices = new Uint16Array(numPoints);
+    //     const offsets = new Float32Array(numPoints * 3);
+    //     const angles = new Float32Array(numPoints);
+
+    //     for (let i = 0; i < numPoints; i++) {
+    //         offsets[i * 3 + 0] = i % width;
+    //         offsets[i * 3 + 1] = Math.floor(i / width);
+
+    //         indices[i] = i;
+
+    //         angles[i] = Math.random() * Math.PI;
+    //     }
+    //     //geometry.addAttribute('position', positions);
+    //     //geometry.addAttribute('uv', uvs);
+    //     //geometry.addAttribute('pindex', new InstancedBufferAttribute(indices, 1, false));
+    //     //geometry.addAttribute('offset', new InstancedBufferAttribute(offsets, 3, false));
+    //     //geometry.addAttribute('angle', new InstancedBufferAttribute(angles, 1, false));
+
+    //     const ipositions = new InstancedBufferAttribute(positions, 3);
+    //     const iscales = new InstancedBufferAttribute(new Int8Array([0.5]), 1);
+    //     const pIndex
+
+
+
+    //     return {
+    //         index: indexes,
+    //         attribs: {
+    //             iPosition: positions,
+    //             iScale: iscales,
+    //             uvs,
+    //             //...sphere.attributes
+    //         }
+    //     }
+    // }
+
+     const {
         numInstances = 1000,
         dimensions = 3,
         packAttempts = 500,
@@ -16,7 +82,7 @@ export default function instancedBufferGeometry(props) {
     } = props
 
     // Builds instanced data for the packing
-    const objdata = useMemo(() => {
+    const objectData = useMemo(() => {
         let sphere = new SphereGeometry(1, widthSegments, heightSegments);
 
         const settings = {
@@ -30,6 +96,7 @@ export default function instancedBufferGeometry(props) {
 
         // generate scale/radius + positions
         const circles = pack(settings)
+        console.log(circles)
         
         // setup arrays
         let positions = new Float32Array(numInstances * 3)
@@ -59,6 +126,16 @@ export default function instancedBufferGeometry(props) {
             }
         }
     }, [])
+
+    const uniforms = {
+        uTime: { value: 0 },
+        uRandom: { value: 1.0 },
+        uDepth: { value: 2.0 },
+        uSize: { value: 0.0 },
+        uTextureSize: { value: new Vector2(width, height) },
+        uTexture: { value: null },
+        uTouch: { value: null },
+    };
 
 
     const vertex = `
@@ -94,12 +171,15 @@ export default function instancedBufferGeometry(props) {
             glFragColor = vec4(1.0,vScale,0.0,1.);
         }    
     `
+
     return (
         <mesh>
             <instancedBufferGeometry
                 instanceCount={numInstances}
-                index={objdata.index}
-                attributes={objdata.attribs}/>
+                index={objectData.index}
+                attributes={objectData.attribs}
+                uniforms={uniforms}
+            />
             <rawShaderMaterial
                 glslVersion={GLSL3}
                 vertexShader={vertex}
