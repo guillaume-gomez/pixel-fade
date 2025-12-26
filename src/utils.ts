43534bf5037d2497f7e00bd7bada0b1441c2a2cd
Guillaume Gomez <guillaume.gomez846@gmail.com>
2025-12-26 10:05:27 +0100

    linter

--- src/utils.ts
+++ src/utils.ts
@@ -44,8 +44,7 @@ export function resizeImage(image: HTMLImageElement, expectedWidth: number, expe
 
   canvasBuffer.width = image.width;
   canvasBuffer.height = image.height;
-
-  contextBuffer.drawImage(image, 0, 0, image.width, image.height);
+  contextBuffer.drawImage(image, 0, 0, canvasBuffer.width ,  canvasBuffer.height);
 
   const canvasTarget = document.createElement("canvas");
 
@@ -69,4 +68,49 @@ function componentToHex(c: number) : string {
 
 export function rgbToHex(r: number, g: number, b: number) : string {
   return "#" + componentToHex(Math.floor(r)) + componentToHex(Math.floor(g)) + componentToHex(Math.floor(b));
+}
+
+export function getAverageBackground(image: HTMLImageElement) : string {
+  const canvasBuffer = document.createElement("canvas");
+  const contextBuffer = getContext(canvasBuffer);
+  
+  canvasBuffer.width = image.width;
+  canvasBuffer.height = image.height;
+  
+  contextBuffer.drawImage(image, 0, 0, image.width, image.height);
+
+  const [red, green, blue] = averageColor(contextBuffer, image.width, image.height);
+  return rgbToHex(...darkColor(red, green, blue, 100));
+}
+
+function darkColor(red : number, green : number, blue: number, amount: number): [number, number, number] { 
+    const clamp = (val : number) : number => Math.max(val, 0);
+    
+    const r = clamp(red - amount)
+    const g = clamp( green - amount)
+    const b = clamp( blue - amount)
+    
+    return [r, g, b];
+}
+
+function averageColor(context: CanvasRenderingContext2D, width: number, height: number) {
+  const imageData = context.getImageData(0, 0, width, height);
+  
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+
+  const numberOfPixels = imageData.data.length/4;
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+   red += imageData.data[i];
+   green += imageData.data[i + 1];
+   blue += imageData.data[i + 2];
+  }
+
+  const redComponent = (red) / numberOfPixels;
+  const greenComponent = (green) / numberOfPixels;
+  const blueComponent = (blue) / numberOfPixels;
+
+  return [redComponent, greenComponent, blueComponent];
 }
