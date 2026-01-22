d7557a775f4ae0f1d7902df0cb452b148ecacc61
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-01-22 09:33:17 +0100

    fix conflict

--- src/Three/FBO/Scene.tsx
+++ src/Three/FBO/Scene.tsx
@@ -1,5 +1,5 @@
-import { OrbitControls, useFBO } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useFBO, RoundedBox } from "@react-three/drei";
+import { Canvas, extend } from "@react-three/fiber";
 import { useRef, useState, Suspense } from "react";
 import { 
   GizmoHelper,
@@ -10,13 +10,16 @@ import {
   useHelper,
   Grid,
 } from '@react-three/drei';
-import { Color } from "three";
+import { Color, Vector2 } from "three";
 import { EffectComposer, Vignette, ChromaticAberration, Bloom, Grid as GridP } from '@react-three/postprocessing';
 import { BlendFunction } from 'postprocessing';
 import SenaarEffect from "../Effect/Senaar/SeenarShaderEffect";
 import Range from "../../components/Range";
 import FBOParticles from "./FBOParticles";
 import FallbackLoader from "../FallbackLoader";
+import VaranoiMaterial from "../Material/VaranoiMaterial";
+
+extend({ VaranoiMaterial });
 
 const SceneTest = () => {
   const [red, setRed] = useState<number>(1.);
@@ -39,24 +42,38 @@ const SceneTest = () => {
       <ambientLight intensity={0.5} />
       <color attach="background" args={["#0000FF"]} />
       <Stats/>
-      <Suspense fallaback={FallbackLoader}>
-      <FBOParticles red={red} />
-      <Grid args={[1000, 1000]} position={[0,0,0]} cellColor='green' />
-      <GizmoHelper alignment="bottom-right" margin={[100, 100]}>
-        <GizmoViewport labelColor="white" axisHeadScale={1} />
-      </GizmoHelper>
-      <EffectComposer enableNormalPass={false}>
-        <Vignette
-          offset={0.1} darkness={0.8} 
-          eskil={false} // Eskil's vignette technique
-          blendFunction={BlendFunction.NORMAL} // blend mode
-        />
-        {/*<Bloom mipmapBlur luminanceThreshold={1.0} />*/}
-        {/*<ChromaticAberration
-          blendFunction={BlendFunction.NORMAL} // blend mode
-          offset={[0.01, 0.01]} // color offset
-        />*/}
-        <SenaarEffect
+      <color attach="background" args={["#0007A6"]} />
+      <Suspense fallback={<FallbackLoader/>}>
+        
+          <FBOParticles />
+          <RoundedBox
+            
+            args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
+            radius={0.05} // Radius of the rounded corners. Default is 0.05
+            steps={1} // Extrusion steps. Default is 1
+            smoothness={4} // The number of curve segments. Default is 4
+            bevelSegments={4} // The number of bevel segments. Default is 4, setting it to 0 removes the bevel, as a result the texture is applied to the whole geometry.
+            creaseAngle={0.4} // Smooth normals everywhere except faces that meet at an angle greater than the crease angle
+          >
+            {/*<meshPhongMaterial color="#f3f3f3" />*/}
+            <varanoiMaterial
+              uTime={1.0}
+              uResolution={new Vector2(100, 100)}
+              uMouse={new Vector2(1,1)}
+            />
+          </RoundedBox>
+          <Grid args={[1000, 1000]} position={[0,0,0]} cellColor='green' />
+        
+        <GizmoHelper alignment="bottom-right" margin={[100, 100]}>
+          <GizmoViewport labelColor="white" axisHeadScale={1} />
+        </GizmoHelper>
+        <EffectComposer enableNormalPass={false}>
+          <Vignette
+            offset={0.1} darkness={0.8} 
+            eskil={false} // Eskil's vignette technique
+            blendFunction={BlendFunction.NORMAL} // blend mode
+          />
+          <SenaarEffect
           param={{
               color: new Color(0xFF0000),
               enableStripe: true,
@@ -64,6 +81,11 @@ const SceneTest = () => {
               gradiantCurve: 0.5
             }}
         />
+            {/*<Bloom mipmapBlur luminanceThreshold={1.0} />*/}
+            {/*<ChromaticAberration
+              blendFunction={BlendFunction.NORMAL} // blend mode
+              offset={[0.01, 0.01]} // color offset
+            />*/}
         </EffectComposer>
       </Suspense>
       <CameraControls ref={cameraRef} />
