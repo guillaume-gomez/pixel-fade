92e51e557769b9480ad1be55aceca1e7f2e94300
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-01-27 09:31:57 +0100

    add new component to display title of the project

---
+++ src/Three/Intro.tsx
@@ -0,0 +1,26 @@
+import { Text3D, Center } from '@react-three/drei';
+
+function Intro() {
+	return (
+      <Center>
+       <Text3D
+        letterSpacing={-0.06}
+        size={40}
+        font="/Inter_Bold.json"
+        curveSegments={32}
+        bevelEnabled
+        bevelSize={0.04}
+        bevelThickness={5}
+        height={0.5}
+        lineHeight={0.5}
+        letterSpacing={-0.06}
+        visible={true}
+      >
+        Floating pixels
+        <meshStandardMaterial color={"orange"} />
+      </Text3D>
+    </Center>
+   	);               
+}
+
+export default Intro;
