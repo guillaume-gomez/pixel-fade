0efb7f345ea4e28cd73340a07634d75e7b4970a1
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-02-01 13:13:57 +0100

    add title component to play role as a footer

---
+++ src/components/Title.tsx
@@ -0,0 +1,29 @@
+import { useSpring, animated } from '@react-spring/web';
+import { SettingsContext } from "../SettingsContext";
+import { useContext } from 'react';
+
+function Title() {
+	const { timerIntroInMs } = useContext(SettingsContext);
+
+  const springs = useSpring({
+    from: { opacity: 0, right: '0rem', textShadow: "#ffcc00 1px 0 10px",  },
+    to: { opacity: 1, right: '2.5rem' },
+    delay: timerIntroInMs + 500,
+    config: {
+      duration: 1000
+    }
+
+  });
+
+	return (
+		<animated.div
+        className="lg:absolute bottom-10  z-10 text-center"
+        style={springs}
+      >
+         <h1 className="text-5xl">Particle Pixel Ballet</h1>
+         <span className="text-3xl"> By Guillaume Gomez</span>
+      </animated.div>
+	)
+}
+
+export default Title;
