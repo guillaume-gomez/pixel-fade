d5de20957608bc96606648c923cd1026d1f730ff
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-02-28 12:55:48 +0100

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { useState, useContext } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useContext } from 'react';
 import Form from "./components/Form";
 import Title from "./components/Title";
-import { resizeImage, getAverageBackground } from "./utils";
 import ThreejsRenderer from "./Three/ThreejsRenderer";
-import { type GeometryType } from "./Three/InstancedBufferGeometry";
 
 import { SettingsContext } from "./SettingsContext";
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  
   const {
     image,
     geometryType,
@@ -49,26 +42,6 @@ function App() {
         <Form />
       </div>
       
-      <div className="card">
-         <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
