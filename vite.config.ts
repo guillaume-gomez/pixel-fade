cdce1d463428dec89a0db0d615c3e804da9bf469
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-03-01 14:05:45 +0100

    add deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
+  //base: "/pixel-fade",
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    checker({typescript: true}),
   ],
 })
